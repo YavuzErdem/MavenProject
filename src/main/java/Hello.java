bd9faf5f154030322b63f36820c7a790a39d1a71
YavuzErdem <yvzerdem06@gmail.com>
2021-06-15 04:00:25 +0100

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Devoloping hello feature");
+        System.out.println("Github Repo is already published to Github");
+        System.out.println("Doing more work");
     }
 }
